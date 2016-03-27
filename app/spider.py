f4ca74af0fe795686d1aff68737fa0d7d3a391fa
Coderhypo <i@ihypo.net>
2016-03-27 18:02:21 +0800

    update

--- app/spider.py
+++ app/spider.py
@@ -18,7 +18,6 @@ def get_grade_list(studentno):
             'name': soup.body.table.find_all('table')[0].find_all('tr')[1].find_all('td')[1].text,
             'class': soup.body.table.find_all('table')[0].find_all('tr')[1].find_all('td')[6].text
         }
-        print "[find]: Student:" + info['no'] + " Name:" + info['name'] + " Class:" + info['class']  
     except:
         return None
 
@@ -51,7 +50,7 @@ def get_grade_list(studentno):
 
         gradelist.append(tmp)
 
-
+        print "[Student] No:" + info['no'] + " Name:" + info['name'] + " Class:" + info['class']  
     return {
         'info': info,
         'gradelist': gradelist
